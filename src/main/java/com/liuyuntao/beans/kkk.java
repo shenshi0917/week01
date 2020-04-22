440f7d3bf5e625392832f6c05506a234281cadca
liuyuntao <liuyuntao@example.com>
2020-04-22 20:40:16 +0800

    tianjialyigeziduan

---
+++ src/main/java/com/liuyuntao/beans/kkk.java
@@ -0,0 +1,34 @@
+package com.liuyuntao.beans;
+
+public class kkk {
+	private int kid;
+	private String kname;
+	private int age;
+	public kkk() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getKid() {
+		return kid;
+	}
+	public void setKid(int kid) {
+		this.kid = kid;
+	}
+	public String getKname() {
+		return kname;
+	}
+	public void setKname(String kname) {
+		this.kname = kname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "kkk [kid=" + kid + ", kname=" + kname + ", age=" + age + "]";
+	}
+	
+}
